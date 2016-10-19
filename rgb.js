843cc5043a0b1649198de89536fdf9ab13874dd0
Wozacosta <srabah-m@student.42.fr>
2016-10-19 08:31:39 +0200

    Added comments + cleaned up css and javascript files

--- rgb.js
+++ rgb.js
@@ -1,15 +1,13 @@
 var colorToGuess = getRandomColor();
 var inGame = true;
-var randomDivNumber;
+var randomDivNumber; /* Gives us which div square is the correct one, 0-2 or 0-5 depending on easy/hard mode */
+var colorDivs; /* Coloured Div Squares */
+var nbOfSquares; /* Number of square to guess from */
 
-/* Coloured Div Squares */
-var colorDivs;
-
-/* Number of square to guess from */
-var nbOfSquares;
-
+/* First run of the game when page loads */
 play();
 
+/* Main function */
 function play(){
     inGame = true;
     colorToGuess = getRandomColor();
@@ -24,45 +22,32 @@ function play(){
     document.querySelector("#green").textContent = colorToGuess.g;
     document.querySelector("#blue").textContent = colorToGuess.b;
 
-    console.log("color2guess = " + colorToGuess.r + "," + colorToGuess.g + "," + colorToGuess.b);
-
-
-    /*
-     ASSIGNS RANDOM COLORS TO DIV
-     */
+    /*  Assigns random colors to the square divs */
     for (var div of colorDivs){
         var newColor;
         newColor = getRandomColor();
         div.style.background = newColor.hexa;
     }
-    /*
-     Reassign correct colorToGuess to a random div square
-     */
+
+    /*  Reassign correct colorToGuess to a random div square   */
     randomDivNumber = Math.floor(Math.random() * nbOfSquares);
     colorDivs[randomDivNumber].style.background = colorToGuess.hexa;
-    /*
-     Set events when user clicks on a square
-     */
 
+    /*  Set events when user clicks on a square  */
     for (let i = 0; i < colorDivs.length; i++){
-            colorDivs[i].addEventListener("click", function(){
+        colorDivs[i].addEventListener("click", function(){
+            if (inGame) { // Check if we're in a game, problems arised without it when the game was over and we could still click on colored divs.
+                if (i != randomDivNumber)
+                    this.classList.add("clicked");
+                else
+                    won();
+            }
+        });
+    }
 
-                if (inGame) { // Check if we're in a game, problems arised without it when the game was over and we could still click on colored divs.
-                    // console.log("inside event listener i = " + i + ", randomDivNumber = " + randomDivNumber);
-                    if (i != randomDivNumber) {
-                        this.classList.add("clicked");
-                    }else{
-                        won();
-                    }
-                }
-            });
-        }
-        console.log("end of play");
 }
 
-
-
-
+/* After player clicks on the correct div square */
 function won(){
     if (inGame) { // Check if we're playing the game.
         document.querySelector("#message").innerHTML = "<span class=\"display-large\">Correct!</span>";
@@ -76,19 +61,23 @@ function won(){
     }
 }
 
-/*
-Manages the NEW COLORS / PLAY AGAIN
-*/
+/*  Manages the NEW COLORS / PLAY AGAIN   */
 var newGame = document.querySelector("nav > p");
 newGame.addEventListener("click", play);
 
-/*
-Manages easy / hard
- */
+/*   Manages easy / hard
+*       the class .row : display the colored divs + adds them to colorDivs
+*       if easy -> only first row gets a .row class
+*       if hard -> both rows get a .row class
+*
+*       + gives the EASY/HARD button a custom style (through class .selected) if selected
+* */
 var difficulties = document.querySelectorAll("#difficulty p"); /* 0 = easy, 1 = hard */
+
+/* EASY button click handler */
 difficulties[0].addEventListener("click", function(){
-   difficulties[1].classList.remove("selected");
-   difficulties[0].classList.add("selected");
+    difficulties[1].classList.remove("selected");
+    difficulties[0].classList.add("selected");
 
     // Displays only first row
     var secondRow = document.querySelectorAll(".container > div")[1];
@@ -97,9 +86,10 @@ difficulties[0].addEventListener("click", function(){
     for (var div of colorDivs) {
         div.classList.remove("clicked");
     }
-    play();
+    play(); // launch a new game with easy settings.
 });
 
+/* HARD button click handler */
 difficulties[1].addEventListener("click", function(){
    difficulties[0].classList.remove("selected");
     difficulties[1].classList.add("selected");
@@ -111,13 +101,11 @@ difficulties[1].addEventListener("click", function(){
     for (var div of colorDivs) {
         div.classList.remove("clicked");
     }
-    play();
+    play(); // launch a new game with hard settings
 });
 
 
-/*
-UTIL FUNCTIONS
- */
+/* -------------- UTIL FUNCTIONS ----------- */
 
 function getRandomOctet(){
     return Math.floor(Math.random() * (256));
@@ -130,17 +118,16 @@ function pad(colorString){
     }
     return colorString;
 }
-/*Returns an objects with 4 properties
-r, g, b -> 0-255 number
-hexa    -> string formatted representation of that color (eg. "#a8a8b2")
- */
 
+/*      Returns an objects with 4 properties
+        r, g, b -> 0-255 number
+        hexa    -> string formatted representation of that color (eg. "#a8a8b2")   */
 function getRandomColor(){
     var color = {};
     color.r = getRandomOctet();
     color.g = getRandomOctet();
     color.b = getRandomOctet();
-    var redString = pad(color.r.toString(16)); // we had padding (ie. leading zeros) -->for instance cases where red value is 7 messes up the final result, we want 07 instead.
+    var redString = pad(color.r.toString(16));
     var greenString = pad(color.g.toString(16));
     var blueString = pad(color.b.toString(16));
     color.hexa = "#" + redString + greenString + blueString;
